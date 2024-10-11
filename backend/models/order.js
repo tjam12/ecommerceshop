0de1564044f18f256f4307b6c5b890fb74b8a047
Paisen <izansazziti3@gmail.com>
2024-10-11 20:44:47 +0800

    adding orders schema to data model

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,9 +1,65 @@
 const mongoose = require('mongoose');
 
 const ordersSchema = mongoose.Schema({
-    name: String,
-    id: Number,
-    countInStock: Number
+    
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'OrderItem',
+        required:true
+    }],
+    shippingAddress1: {
+        type: String,
+        required:true
+    },
+    shippingAddress2: {
+        type: String,
+        required: false
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone : {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending'
+    },
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',//follow mongoose schema naming, not exports
+        required:true
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now
+    }
 })
+
+
+ordersSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+}
+)
+
+ordersSchema.set('toJSON', {
+    virtuals: true,
+});
+
 
 exports.Orders = mongoose.model('Orders', ordersSchema);
