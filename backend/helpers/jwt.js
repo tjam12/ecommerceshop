bb4a09fd7f8fbd842b068f54d4d230d179d95475
Paisen <izansazziti3@gmail.com>
2024-10-11 16:07:17 +0800

    Adding admin permission checking for token validation

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,11 +1,39 @@
 const { expressjwt: jwt } = require('express-jwt'); // Correct import for express-jwt v7+
 
-
 function authJWT() {
+    const Secret = process.env.secret;
+    const Api = process.env.API_URL;
+
     return jwt({
-        secret: process.env.secret,
+        secret: Secret,
         algorithms: ["HS256"],
+        isRevoked: isRevoked
+      }).unless({
+        path:[
+            `${Api}/users/login`,
+            `${Api}/users/register`,
+            {
+                url: /\/api\/v1\/products(.*)/, //regex expression
+                methods: ['GET', 'OPTIONS']
+            },
+            {
+                url: /\/api\/v1\/categories(.*)/, //regex expression
+                methods: ['GET', 'OPTIONS']
+            }
+        ]
       });
+}
+
+async function isRevoked(req, jwt){
+
+    const payload = jwt.payload;
+
+    if(!payload.isAdmin) {
+        return true
+    }
+    else{
+        return false
+    }
 }
 
 module.exports = authJWT;
